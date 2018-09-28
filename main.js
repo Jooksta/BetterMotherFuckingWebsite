7998db49da408fd4825bf640160d0f2211d133ed
Jooksta <teslachipset@gmail.com>
2018-09-28 18:06:55 +0200

    ajout fichier main.js avec popup

---
+++ main.js
@@ -0,0 +1,4 @@
+setTimeout (function() {alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+if (confirm("Clique sur Ok pour aller sur xxxvidsxxx ;)"))
+location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}, 10000);
